42570480e58168a82b36b8c85bc093272bdc7a06
mindaugasbukauskas1996-ship-it <mindaugas.bukauskas1996@gmail.com>
2026-03-05 11:49:54 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,12 @@
 import os
 import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_URL = os.getenv("SUPABASE_DB_URL")
 
 def get_conn():
-    return psycopg.connect(DB_URL)
+    if not DB_URL:
+        raise RuntimeError("SUPABASE_DB_URL nenustatytas (Render Environment).")
+    return psycopg.connect(DB_URL, sslmode="require")
